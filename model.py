aedde4544b44a7416290dd676e182dd062428a88
Michal Gruszka <m.gruszka215@gmail.com>
2017-11-05 20:52:04 +0100

    Change tasks attribute to private, create getters

--- model.py
+++ model.py
@@ -4,29 +4,40 @@ from task import Task
 class Model:
 
     def __init__(self):
-        self.tasks = []
+        self._tasks = []
+
+    def get_task(self, index):
+        if index in range(0, len(self._tasks)):
+            return self._tasks[index]
+        else:
+            raise IndexError('Invalid index value.')
+
+    def has_no_tasks(self):
+        if len(self._tasks):
+            return False
+        return True
 
     def add_task(self, task):
         if isinstance(task, Task):
-            self.tasks.append(task)
+            self._tasks.append(task)
         else:
             raise TypeError("'task' must be an instance of Task class.")
 
     def delete_task(self, index):
-        if index in range(0, len(self.tasks)):
-            del self.tasks[index]
+        if index in range(0, len(self._tasks)):
+            del self._tasks[index]
         else:
-            raise ValueError('Invalid index value.')
+            raise IndexError('Invalid index value.')
 
-    def mark_task_as_done(self, index):
-        if not self.tasks[index].is_done:
-            self.tasks[index].is_done = True
+    def mark_unmark_task(self, index):
+        task = self.get_task(index)
+        task.toggle_is_done()
 
     def __str__(self):
-        if len(self.tasks) == 0:
+        if len(self._tasks) == 0:
             return '-- The list is empty --'
         string = ''
         i = 0
-        for task in self.tasks:
-            string += '({}) {}: {}\n'.format(i, task.get_name(), task.get_description())
+        for task in self._tasks:
+            string += '({}) [{}] {}\n'.format(i, 'X' if task.get_is_done() else ' ', task.get_name())
         return string
